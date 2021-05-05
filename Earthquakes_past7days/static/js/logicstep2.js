82c2fca1ecb4da43ca9ea0bb481e9038940a43a2
merin_anto <merinanto@gmail.com>
2021-05-05 16:56:51 -0700

    adding earthquake legend

--- Earthquakes_past7days/static/js/logicstep2.js
+++ Earthquakes_past7days/static/js/logicstep2.js
@@ -34,18 +34,8 @@ let map = L.map('mapid', {
     layers: [satelliteStreets]
 })
 
-// Create the earthquake layer for our map.
-let earthquakes = new L.layerGroup();
-
-// We define an object that contains the overlays.
-// This overlay will be visible all the time.
-let overlays = {
-  Earthquakes: earthquakes
-};
-
-// Then we add a control to the map that will allow the user to change
-// which layers are visible.
-L.control.layers(baseMaps, overlays).addTo(map);
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 
 // This function returns the style data for each of the earthquakes we plot on
@@ -110,8 +100,6 @@ d3.json(earthquakePlot).then(function(data) {
             onEachFeature: function(feature, layer) {
                 layer.bindPopup( "Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
                 }
-        }).addTo(earthquakes);
-        //Then we add the earthquake layer to our map.
-        earthquakes.addTo(map);
+        }).addTo(map);
 });
 
--- Earthquakes_past7days/static/js/logicstep5.js
+++ Earthquakes_past7days/static/js/logicstep5.js
@@ -97,6 +97,7 @@ function getColor(magnitude) {
 // Accessing the Toronto neighborhoods GeoJSON URL.
 let earthquakePlot = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 // Retrieve the earthquake GeoJSON data.
+
 d3.json(earthquakePlot).then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
@@ -113,5 +114,34 @@ d3.json(earthquakePlot).then(function(data) {
         }).addTo(earthquakes);
         //Then we add the earthquake layer to our map.
         earthquakes.addTo(map);
+       
+ 
 });
 
+// Create a legend control object.
+let legend = L.control({
+  position: "bottomright"
+  });
+// Then add all the details for the legend.
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div", "info legend");
+  for (var i = 0; i < magnitudes.length; i++) {
+    console.log(colors[i]);
+    div.innerHTML +=
+    "<i style='background: " + colors[i] + "'></i> " +
+    magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const colors = [
+    "#98ee00",
+    "#d4ee00",
+    "#eecc00",
+    "#ee9c00",
+    "#ea822c",
+    "#ea2c2c"
+  ];
+  // Looping through our intervals to generate a label with a colored square for each interval.
+  legend.addTo(map);   
+  
